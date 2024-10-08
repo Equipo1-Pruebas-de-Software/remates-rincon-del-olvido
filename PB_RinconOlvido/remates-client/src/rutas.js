30c42097cfdb5b37dc312735e495a9ee3e0d6d60
Nicolas <nicolas.olivosm@usm.cl>
2024-10-08 18:09:38 -0300

    Front-end de ofertar
    Front para visualizar mayor detalle de los productos y simular ofertar de manera local

--- PB_RinconOlvido/remates-client/src/rutas.js
+++ PB_RinconOlvido/remates-client/src/rutas.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Routes, useLocation} from 'react-router-dom';
 import Catalogo from './Catalogo';
 import Header from './Header';
+import ProductoDetalle from './Detalleproducto';
 
 function Rutas() {
   const location = useLocation(); //Para obtener ruta actual
@@ -12,6 +13,7 @@ function Rutas() {
       <Header isLogin={location.pathname === '/login' || location.pathname === '/register'}/>
       <Routes>
         <Route path="/" element={<Catalogo />} />
+        <Route path="/producto/:id" element={<ProductoDetalle />} />
       </Routes>
     </>
   );
